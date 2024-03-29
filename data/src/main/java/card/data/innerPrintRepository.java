72cb38eb2f6586a22fd7738bae06878872507a3d
sheibuzhongyao <127662213+sheibuzhongyao@users.noreply.github.com>
2024-03-29 18:14:27 +0800

    Delete data directory

--- data/src/main/java/card/data/innerPrintRepository.java
+++
@@ -1,9 +0,0 @@
-package card.data;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-import card.domain.innerPrint;
-
-public interface innerPrintRepository extends ReactiveCrudRepository<innerPrint, Long>{
-    
-}
--- data/src/main/java/card/data/skillRepository.java
+++
@@ -1,9 +0,0 @@
-package card.data;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-import card.domain.skill;
-
-public interface skillRepository extends ReactiveCrudRepository<skill, Long> {
-    
-}
