00a6f9100b7f1d67b754f14348406e0c01fc092d
sheibuzhongyao <127662213+sheibuzhongyao@users.noreply.github.com>
2024-03-29 18:10:44 +0800

    Delete src directory

--- src/main/java/card/data/sanguoshaCardRepository.java
+++
@@ -1,8 +0,0 @@
-package card.data;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import card.domain.sanguoshaCard;
-
-public interface sanguoshaCardRepository extends ReactiveCrudRepository<sanguoshaCard, Long> {
-    
-} 
--- src/main/java/card/data/yugiohCardRepository.java
+++
@@ -1,9 +0,0 @@
-package card.data;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-import card.domain.yugiohCard;
-
-public interface yugiohCardRepository extends ReactiveCrudRepository<yugiohCard, Long>{
-    
-}
